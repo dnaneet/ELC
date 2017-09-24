32486380b277c0f47848d080c838a3f7150882ad
Aneet D Narendranath <dnaneet@mtu.edu>
2017-09-24 17:07:59 -0400

    Add files via upload

--- coach_hour_tracking/compare2.m
+++ coach_hour_tracking/compare2.m
@@ -12,5 +12,20 @@ for i=1:1:nCoach
     actual{i} = data{i}(end,2*week+1);
     error{i} = expected{i} - actual{i};
 end
-plot(cell2mat(error))
-xticks([1:18])
+
+lab={};%Create empty cell for plot labels
+for i=1:1:nCoach
+    s=f(i).name;
+    ix=strfind(s,'.csv');
+    %s(1:ix-1) %Assertion statement for debugging only.
+    lab=[lab, s(1:ix-1)];
+end
+plot(cell2mat(error), 'k*-')
+grid on;
+axis([1 18 -5 5]);
+%After plotting, change the number of x-ticks. 
+xticklabels( {'Adam A.','Anna W.','Brittany S.','Daanish T.','David A.','Elizabeth B.','Erica J.','Jacob P.','Jesse L.','John S.','Julia S.','Marcello G.','Max E.','Paras G.','Riley S.','Robbie T.','Robert M.','Siddhant D.'})
+
+%% Check for conservation of hours for this week
+
+%% Cost this week (Sum(coach_actual * salary_hour, nCoach))
